dc15cfc675ad3a92ea41c86c3ecc49da91aacc67
helenemartin <helenemartin13@hotmail.com>
2017-12-18 19:44:41 +0000

    make canvas bigger than background

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var layer;
 var frame;
 
 function setup() {
-  canvas = createCanvas(windowWidth,windowHeight);
+  canvas = createCanvas(1800, 1600);
   // layer = createVideo(['assets/5.mp4']);
   // layer.hide(); // by default video shows up in separate dom
   //                 // element. hide it and draw it to the canvas
